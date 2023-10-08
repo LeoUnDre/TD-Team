d8afbe0444a47798fe76856999d577b1a5f7a373
V1tazy <132578253+V1tazy@users.noreply.github.com>
2023-10-08 21:52:27 +0900

    0.0.2
    First work spawn and i try to realize a range enemy

--- Assets/Vitazy/Player/Gun.cs
+++ Assets/Vitazy/Player/Gun.cs
@@ -5,25 +5,46 @@ using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
-    [SerializeField]public Transform bulletSpawner;
     [SerializeField]public GameObject objbullet;
 
     public GameObject objclone;
-    public int power = 500;
+    public int power = 1000;
 
-    public void Shoot()
+    private float shootCooldow;
+    private float startShootCooldown = 2f;
+
+    public void Shoot(GameObject types, Transform bulletSpawner)
     {
-        Ray ray = new Ray(bulletSpawner.position, bulletSpawner.forward);
-        RaycastHit hit;
-        float shootdistance = 20f;
-        if (Physics.Raycast(ray, out hit, shootdistance))
-            shootdistance = hit.distance;
+        if (types.GetComponent<TowerControl>() != null)
+        {
+            objbullet.GetComponent<Bullet>().isEnemy = false;
+            Ray ray = new Ray(bulletSpawner.position, bulletSpawner.forward);
+            RaycastHit hit;
+            float shootdistance = 20f;
+            if (Physics.Raycast(ray, out hit, shootdistance))
+                shootdistance = hit.distance;
 
-        objclone = Instantiate(objbullet, bulletSpawner.position, Quaternion.identity);
-        objclone.GetComponent<Rigidbody>().AddForce(bulletSpawner.transform.forward * power);
-        Destroy(objclone, 10);
+            objclone = Instantiate(objbullet, bulletSpawner.position, Quaternion.identity);
+            objclone.GetComponent<Rigidbody>().AddForce(bulletSpawner.transform.forward * power);
+            Destroy(objclone, 10);
 
 
-        Debug.DrawRay(ray.origin, ray.direction * shootdistance, Color.blue, 1);
+            Debug.DrawRay(ray.origin, ray.direction * shootdistance, Color.blue, 1);
+        }
+        else if (types.GetComponent<RangeEnemy>() != null)
+        {
+            if (shootCooldow <= 0)
+            {
+                objbullet.GetComponent<Bullet>().isEnemy = true;
+                objclone = Instantiate(objbullet, bulletSpawner.position, Quaternion.identity);
+                objclone.GetComponent<Rigidbody>().AddForce(bulletSpawner.transform.forward * power);
+                Destroy(objclone, 10);
+                shootCooldow = startShootCooldown;
+            }
+
+            shootCooldow -= Time.deltaTime;
+        }
+
+
     }
 }
